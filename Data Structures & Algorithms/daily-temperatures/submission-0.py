cc0c3398049c37c9d687fb4d34d78c4a1c6a939d
Jiaxuan Li <64050351+Chenguku@users.noreply.github.com>
2026-04-05 00:36:10 -0400

    Update: daily-temperatures - submission-0

--- Data Structures & Algorithms/daily-temperatures/submission-0.py
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -1,10 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0] * len(temperatures)
+        results = [0] * len(temperatures)
         stack = []
-        for i, j in enumerate(temperatures):
-            while stack and j > stack[-1][0]: #index of last number
-                stackTemp, stackIndex = stack.pop()
-                result[stackIndex] = i - stackIndex
-            stack.append((j, i))
-        return result
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > stack[len(stack) - 1][1]:
+                index = stack.pop()[0]
+                results[index] = i - index
+            stack.append((i, temperatures[i]))
+        return results
